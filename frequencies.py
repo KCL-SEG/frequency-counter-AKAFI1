4c156c92b6f396f8bd1478c671bcda7b50158eb5
AKAFI1 <99483741+AKAFI1@users.noreply.github.com>
2022-10-03 17:18:11 +0100

    finished e1.2

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,22 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+from sre_compile import isstring
+
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+    
+        if type(item) != str:
+            item = str(item)
+
+        if item in frequencies:
+            frequencies[item] += 1
+
+        else:
+            frequencies[item] = 1
+
     return frequencies
+
+"""test"""
+print(frequencies(['a', 'a', 'b', 'c', 2, 'a', True]))
